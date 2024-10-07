487203b8ae82aa645b06de776d19489f6e094a77
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-10-07 01:07:08 +0100

    tiny oversights being redeemed

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -14,7 +14,7 @@ const NavLink = ({children, href}: NavLinkProps) => {
     const isActive = pathname === href;
 
     return ( 
-        <Link href={href} className="hover:bg-[#633cff]">
+        <Link href={href} className="hover:text-[#633cff]">
             <div className={`flex gap-2 items-center justify-center px-5 py-2 ${isActive? 'bg-[#efebff] text-[#633cff] rounded-lg font-semibold':''}`}>
                 {children}
             </div>
